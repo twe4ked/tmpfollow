583baec2caa1f5b97ae72a3a96e9a5a4493edd0e
Odin Dutton <odindutton@gmail.com>
2012-05-19 16:31:21 +1000

    Clean up `rspec --format documentation` output.

--- spec/lib/fixnum_spec.rb
+++ spec/lib/fixnum_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe Fixnum do
   describe 'ordinalize' do
-    [
-      [1, '1st'],
-      [2, '2nd'],
-      [3, '3rd'],
-      [4, '4th'],
-      [113, '113th']
-    ].each do |num, expected|
-      it "#{num} should be #{expected}" do
-        num.ordinalize.should == expected
+    it 'ordinalizes numbers correctly' do
+      [
+        [1, '1st'],
+        [2, '2nd'],
+        [3, '3rd'],
+        [4, '4th'],
+        [113, '113th']
+      ].each do |num, expected|
+          num.ordinalize.should == expected
       end
     end
   end
